d450efdb0723d4e71195c6540f08e7e87b22cd0e
William <flintman@flintmancomputers.com>
2025-10-30 14:08:15 -0400

    cookies:  don't save password, Give me a token to use

--- db_init/schema.sql
+++ db_init/schema.sql
@@ -111,6 +111,7 @@ CREATE TABLE IF NOT EXISTS `flintmancms_profile` (
   `sign_date` varchar(45) NOT NULL,
   `permissions` int(11) NOT NULL,
   `active` int(11) NOT NULL,
+  `remember_token` VARCHAR(255) DEFAULT NULL,
   `hash` varchar(75) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
--- html/includes/auth.php
+++ html/includes/auth.php
@@ -26,45 +26,70 @@ function login() {
     if (isset($_POST['username'])) {
         $username = strtolower(scrub_input($_POST['username']));
         $password = md5(scrub_input($_POST['password']));
-    } elseif (isset($_COOKIE['username']) && isset($_COOKIE['password'])) {
+        $remember_token = null;
+    } elseif (isset($_COOKIE['username']) && isset($_COOKIE['remember_token'])) {
         $username = $_COOKIE['username'];
-        $password = $_COOKIE['password'];
+        $remember_token = $_COOKIE['remember_token'];
+        $password = null;
     } else {
         logout();
     }
 
-    $sql = "SELECT * FROM " . DB_PREFIX . "_profile";
+    $escaped_username = mysqli_real_escape_string($db->db_connect_id, $username);
+    $sql = "SELECT * FROM " . DB_PREFIX . "_profile WHERE username='" . $escaped_username . "'";
     $result = $db->sql_query($sql)
-            or $errorMsg = "ERROR: " . $db->sql_error(). " @ Line " . __LINE__ . " Of " . __FILE__;
-    ;
+        or $errorMsg = "ERROR: " . $db->sql_error(). " @ Line " . __LINE__ . " Of " . __FILE__;
 
-    while ($data = $db->sql_fetchrow($result)) {
-
-        if ($username == strtolower($data['username']) && $password == $data['password']) {
-
+    $data = $db->sql_fetchrow($result);
+    if ($data) {
+        if ($password !== null && $password == $data['password']) {
+            // Password login, generate new token if autologin requested
             if (isset($_POST['autologin'])) {
-
-                // they want automatic logins so set the cookie
-                // set to expire in one month
+                $new_token = bin2hex(random_bytes(32));
                 setcookie('username', strtolower($data['username']), time() + 2629743);
-                setcookie('password', $data['password'], time() + 2629743);
+                setcookie('remember_token', $new_token, time() + 2629743);
+                // Save token in DB
+                $escaped_token = mysqli_real_escape_string($db->db_connect_id, $new_token);
+                $update_sql = "UPDATE " . DB_PREFIX . "_profile SET remember_token='" . $escaped_token . "' WHERE id=" . (int)$data['id'];
+                $db->sql_query($update_sql);
+            } else {
+                // Remove any old token
+                setcookie('remember_token', '', time() - 3600);
+                $update_sql = "UPDATE " . DB_PREFIX . "_profile SET remember_token=NULL WHERE id=" . (int)$data['id'];
+                $db->sql_query($update_sql);
             }
-
-            // set user variables
-            $_SESSION['username'] = $data['username'];
-            $_SESSION['user_logged_in'] = 1;
-            $_SESSION['profile_id'] = $data['id'];
-            $_SESSION['priv'] = $data['permissions'];
-            $_SESSION['vercode'] = '';
-
-
-            return 1;
+        } elseif ($remember_token !== null && $remember_token === $data['remember_token'] && !empty($remember_token)) {
+            // Token login, rotate token
+            $new_token = bin2hex(random_bytes(32));
+            setcookie('username', strtolower($data['username']), time() + 2629743);
+            setcookie('remember_token', $new_token, time() + 2629743);
+            $escaped_token = mysqli_real_escape_string($db->db_connect_id, $new_token);
+            $update_sql = "UPDATE " . DB_PREFIX . "_profile SET remember_token='" . $escaped_token . "' WHERE id=" . (int)$data['id'];
+            $db->sql_query($update_sql);
+        } else {
+            // Invalid login
+            return 0;
         }
+
+        // set user variables
+        $_SESSION['username'] = $data['username'];
+        $_SESSION['user_logged_in'] = 1;
+        $_SESSION['profile_id'] = $data['id'];
+        $_SESSION['priv'] = $data['permissions'];
+        $_SESSION['vercode'] = '';
+        return 1;
     }
     return 0;
 }
 
 function logout() {
+
+    // Remove token from DB if possible
+    if (isset($_SESSION['profile_id']) && $_SESSION['profile_id'] > 0) {
+        global $db;
+        $update_sql = "UPDATE " . DB_PREFIX . "_profile SET remember_token=NULL WHERE id=" . (int)$_SESSION['profile_id'];
+        $db->sql_query($update_sql);
+    }
 
     // unset the session and remove all cookies
     unset($_SESSION['initiated']);
@@ -74,7 +99,8 @@ function logout() {
     unset($_SESSION['priv']);
     unset($_SESSION['vercode']);
     setcookie('username', '', time() - 2629743);
-    setcookie('password', '', time() - 2629743);
+    setcookie('remember_token', '', time() - 2629743);
+
     $_SESSION['initiated'] = true;
     $_SESSION['username'] = 'Anonymous';
     $_SESSION['session_logged_in'] = 0;
@@ -90,7 +116,7 @@ $_SESSION['name'] = $session_name;
 
 // set session defaults
 if (!isset($_SESSION['initiated'])) {
-    if (isset($_COOKIE['username']) && isset($_COOKIE['password'])) {
+    if (isset($_COOKIE['username']) && isset($_COOKIE['remember_token'])) {
         login();
     } else {
         session_regenerate_id();
