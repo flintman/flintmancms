8e8c18c1bbbd10ba04bdfeacd8e4d38fd79635b8
William <flintman@flintmancomputers.com>
2025-10-31 07:11:26 -0400

    SmaryMenu: drop it,  its old and outdated and doesn't work

--- html/includes/menu/menu.php
+++ html/includes/menu/menu.php
@@ -1,96 +1,59 @@
+
 <?php
+// Basic menu array structure for Smarty foreach in footer.htm
+$menu = array();
 
-/* * *************************************************************************
- *  Copyright (C) 2010  William Bellavance
- *                      Flintman Computers
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * ************************************************************************* */
-
-
-$sm = new SmartyMenu();
-$sm->initMenu($menu);
-
-//Gets all pages names and puts in menu
-
-$sm->initItem($item);
-$sm->setItemText($item, HOME_TEXT);
-$sm->setItemLink($item, "index.php");
-$item['new_window'] = 0;
-$sm->addMenuItem($menu, $item);
+$menu[] = array(
+    'text' => HOME_TEXT,
+    'link' => 'index.php',
+    'new_window' => 0
+);
 
 if ($_SESSION['user_logged_in']) {
-    $sm->initItem($item);
-    $sm->setItemText($item, PROFILE_TEXT);
-    $sm->setItemLink($item, "index.php?n=profile" . $config['add_link']);
-    $item['new_window'] = 0;
-    $sm->addMenuItem($menu, $item);
+    $menu[] = array(
+        'text' => PROFILE_TEXT,
+        'link' => 'index.php?n=profile' . $config['add_link'],
+        'new_window' => 0
+    );
 }
 
 $sql = "SELECT * FROM " . DB_PREFIX . "_links WHERE sub_link='0' ORDER BY `link_order` ASC";
 $result = $db->sql_query($sql);
-
 while ($data = $db->sql_fetchrow($result)) {
-    $display = false;
-    $submenu = false;
-    $sm->initMenu($sub);
-    $display = check_menu($_SESSION['priv'], $data['id']);
-
-    if (!$display) {
-        $check = strpos($data['link'], "page");
-        $checktwo = strpos($data['link'], "plugins");
-        if (!$check && !$checktwo)
-            $display = true;
-    }
-
+    $submenu = array();
     $sql3 = sprintf("SELECT * FROM " . DB_PREFIX . "_links WHERE sub_link=%s",
-                    quote_smart($data['id']));
+        quote_smart($data['id']));
     $result3 = $db->sql_query($sql3);
     while ($data3 = $db->sql_fetchrow($result3)) {
-        $submenu = true;
-        $sm->initItem($item);
-        $sm->setItemText($item, ucwords($data3['name']));
-        $sm->setItemLink($item, $data3['link'] . $config['add_link']);
-        $item['new_window'] = isset($data3['new_window']) ? $data3['new_window'] : 0;
-        $sm->addMenuItem($sub, $item);
+        $submenu[] = array(
+            'text' => ucwords($data3['name']),
+            'link' => $data3['link'] . $config['add_link'],
+            'new_window' => isset($data3['new_window']) ? $data3['new_window'] : 0
+        );
     }
-
-    if ($data['active'] && $display) {
-        $sm->initItem($item);
-        $sm->setItemText($item, $data['name']);
-        $sm->setItemLink($item, $data['link'] . $config['add_link']);
-        $item['new_window'] = isset($data['new_window']) ? $data['new_window'] : 0;
-        if ($submenu)
-            $sm->setItemSubmenu($item, $sub);
-        $sm->addMenuItem($menu, $item);
+    $add = false;
+    if ($data['active'] && check_menu($_SESSION['priv'], $data['id'])) {
+        $add = true;
     } elseif ($_SESSION['priv'] == "1") {
-        $sm->initItem($item);
-        $sm->setItemText($item, $data['name']);
-        $sm->setItemLink($item, $data['link'] . $config['add_link']);
-        $item['new_window'] = isset($data['new_window']) ? $data['new_window'] : 0;
-        if ($submenu)
-            $sm->setItemSubmenu($item, $sub);
-        $sm->addMenuItem($menu, $item);
+        $add = true;
     }
-    $sub = '';
+    if ($add) {
+        $item = array(
+            'text' => $data['name'],
+            'link' => $data['link'] . $config['add_link'],
+            'new_window' => isset($data['new_window']) ? $data['new_window'] : 0
+        );
+        if (count($submenu) > 0) {
+            $item['submenu'] = $submenu;
+        }
+        $menu[] = $item;
+    }
 }
 if ($_SESSION['priv'] == "1") {
-    $sm->initItem($item);
-    $sm->setItemText($item, ADMIN_TEXT);
-    $sm->setItemLink($item, "admin.php");
-    $item['new_window'] = 0;
-    $sm->addMenuItem($menu, $item);
+    $menu[] = array(
+        'text' => ADMIN_TEXT,
+        'link' => 'admin.php',
+        'new_window' => 0
+    );
 }
 ?>
--- html/includes/menu/menu_admin.php
+++ html/includes/menu/menu_admin.php
@@ -1,93 +1,28 @@
+
 <?php
-/* * *************************************************************************
- *  Copyright (C) 2010  William Bellavance
- *                      Flintman Computers
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * ************************************************************************* */
+// Basic admin menu array structure for Smarty foreach in footer.htm
+$menu = array();
 
-$sm = new SmartyMenu();
-$sm->initMenu($menu);
-$sm->initMenu($config_sub);
+$config_sub = array(
+    array('text' => ADMIN_MENU_USERS_CONFIGURE, 'link' => 'admin.php?n=user', 'new_window' => 0),
+    array('text' => ADMIN_MENU_GROUPS_CONFIGURE, 'link' => 'admin.php?n=groups', 'new_window' => 0),
+    array('text' => ADMIN_MENU_EMAIL_CONFIGURE, 'link' => 'admin.php?n=email', 'new_window' => 0),
+    array('text' => ADMIN_MENU_LINKS, 'link' => 'admin.php?n=links', 'new_window' => 0)
+);
 
-$sm->initItem($item);
-$sm->setItemText($item, ADMIN_MENU_USERS_CONFIGURE);
-$sm->setItemLink($item, "admin.php?n=user", 0);
-$item['new_window'] = 0;
-$sm->addMenuItem($config_sub, $item);
-
-$sm->initItem($item);
-$sm->setItemText($item, ADMIN_MENU_GROUPS_CONFIGURE);
-$sm->setItemLink($item, "admin.php?n=groups", 0);
-$item['new_window'] = 0;
-$sm->addMenuItem($config_sub, $item);
-
-$sm->initItem($item);
-$sm->setItemText($item, ADMIN_MENU_EMAIL_CONFIGURE);
-$sm->setItemLink($item, "admin.php?n=email", 0);
-$item['new_window'] = 0;
-$sm->addMenuItem($config_sub, $item);
-
-$sm->initItem($item);
-$sm->setItemText($item, ADMIN_MENU_LINKS);
-$sm->setItemLink($item, "admin.php?n=links", 0);
-$item['new_window'] = 0;
-$sm->addMenuItem($config_sub, $item);
-
-$sm->initItem($item);
-$sm->setItemText($item, ADMIN_MENU_EXIT_ADMIN);
-$sm->setItemLink($item, "index.php", 0);
-$item['new_window'] = 0;
-$sm->addMenuItem($menu, $item);
+$menu[] = array('text' => ADMIN_MENU_EXIT_ADMIN, 'link' => 'index.php', 'new_window' => 0);
 
 if ($_SESSION['priv'] == "1") {
-    $sm->initItem($item);
-    $sm->setItemText($item, ADMIN_MENU_ADMIN_HOME);
-    $sm->setItemLink($item, "admin.php", 0);
-    $item['new_window'] = 0;
-    $sm->addMenuItem($menu, $item);
-
-    $sm->initItem($item);
-    $sm->setItemText($item, ADMIN_MENU_CONFIGURE);
-    $sm->setItemLink($item, "admin.php?n=config", 0);
-    $item['new_window'] = 0;
-    $sm->setItemSubmenu($item, $config_sub);
-    $sm->addMenuItem($menu, $item);
-
-    $sm->initItem($item);
-    $sm->setItemText($item, ADMIN_MENU_LOGS);
-    $sm->setItemLink($item, "admin.php?n=logs", 0);
-    $item['new_window'] = 0;
-    $sm->addMenuItem($menu, $item);
-
-    $sm->initItem($item);
-    $sm->setItemText($item, ADMIN_MENU_PAGES_ADMIN);
-    $sm->setItemLink($item, "admin.php?n=page", 0);
-    $item['new_window'] = 0;
-    $sm->addMenuItem($menu, $item);
-
-    $sm->initItem($item);
-    $sm->setItemText($item, ADMIN_MENU_PLUGINS_ADMIN);
-    $sm->setItemLink($item, "admin.php?n=plugin", 0);
-    $item['new_window'] = 0;
-    $sm->addMenuItem($menu, $item);
-
-    $sm->initItem($item);
-    $sm->setItemText($item, ADMIN_MENU_HELP);
-    $sm->setItemLink($item, "admin.php?n=help", 0);
-    $item['new_window'] = 0;
-    $sm->addMenuItem($menu, $item);
+    $menu[] = array('text' => ADMIN_MENU_ADMIN_HOME, 'link' => 'admin.php', 'new_window' => 0);
+    $menu[] = array(
+        'text' => ADMIN_MENU_CONFIGURE,
+        'link' => 'admin.php?n=config',
+        'new_window' => 0,
+        'submenu' => $config_sub
+    );
+    $menu[] = array('text' => ADMIN_MENU_LOGS, 'link' => 'admin.php?n=logs', 'new_window' => 0);
+    $menu[] = array('text' => ADMIN_MENU_PAGES_ADMIN, 'link' => 'admin.php?n=page', 'new_window' => 0);
+    $menu[] = array('text' => ADMIN_MENU_PLUGINS_ADMIN, 'link' => 'admin.php?n=plugin', 'new_window' => 0);
+    $menu[] = array('text' => ADMIN_MENU_HELP, 'link' => 'admin.php?n=help', 'new_window' => 0);
 }
 ?>
